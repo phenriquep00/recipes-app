67f9b1bc90f8dc01a7f5c11ade518c5654f17665
Pedro Henrique <contact.pedrolima@gmail.com>
2023-02-10 15:18:14 -0300

    update on trending route and bug assertion

--- src/components/Content/TrendingRecipes.tsx
+++ src/components/Content/TrendingRecipes.tsx
@@ -1,29 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Recipe } from "../Recipe/Recipe";
-import axios from "axios";
-
 
 export function TrendingRecipes() {
-  const getTrendingRecipes = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_SPOONACULAR_REACT_API_KEY}&number=8`
-    );
-    const data = await api.json();
+  const [trending, setTrending] = useState<any>([]);
 
-    console.log(data);
+  const checkCached = localStorage.getItem("trending");
+
+  // FIXME: the api request doesn't get setted to the trending state;
+
+  const getTrendingRecipes = async () => {
+    if (checkCached) {
+      console.log(checkCached)
+      
+      setTrending(await JSON.parse(checkCached));
+      console.log(trending)
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${
+          import.meta.env.VITE_SPOONACULAR_REACT_API_KEY
+        }&number=8`
+      );
+      const data = await api.json();
+      console.log(data.recipes)
+
+      localStorage.setItem("trending", JSON.stringify(data.recipes));
+      setTrending(data.recipes);
+      console.log(trending)
+    }
   };
 
   useEffect(() => {
     getTrendingRecipes();
+    
   }, []);
 
   return (
     <section className="w-full flex flex-col items-center pt-5 min-h-[420px] bg-zinc-300 rounded-lg">
       <h1 className="text-gray-500 text-lg font-bold">Trending Recipes</h1>
       <div className="mt-8 flex gap-3">
-        <Recipe />
-        <Recipe />
-        <Recipe />
+        {trending && trending.map(() => {
+          <Recipe />
+        })}
       </div>
     </section>
   );
