d52a0f453d8b5fdee556e90151ed8f41b0482528
Pedro Henrique <contact.pedrolima@gmail.com>
2023-02-17 17:32:12 -0300

    cleared recipes.summary and started working on design for recipe details page

--- src/pages/RecipeDescription.tsx
+++ src/pages/RecipeDescription.tsx
@@ -2,12 +2,12 @@ import { useParams } from "react-router-dom";
 import { Header } from "../components/Header/Header";
 import { useEffect, useState } from "react";
 
-import slugify from "slugify";
+import sanitizeHtml from "sanitize-html";
 import { fetchSingleRecipe } from "../utils/fetchSingleRecipe";
 
 export function RecipeDescription() {
-    const { id } = useParams<{ id: string }>();
-    const [recipe, setRecipe] = useState<any | null>(null);
+  const { id } = useParams<{ id: string }>();
+  const [recipe, setRecipe] = useState<any | null>(null);
 
   useEffect(() => {
     async function getRecipe() {
@@ -27,9 +27,22 @@ export function RecipeDescription() {
   return (
     <main className="flex flex-col items-center min-w-[100vw] min-h-screen gap-10 pb-5 bg-gradient-to-bl from-zinc-100 to-zinc-300">
       <Header />
-      <h1 className="text-black ">{recipe.title}</h1>
-      <p>{recipe.summary}</p>
-      <img src={recipe.image} alt="" />
+      <h1 className="text-black font-bold text-xl">{recipe.title}</h1>
+      <div className="flex gap-4 w-[85%] items-center justify-center">
+        <img
+          className="rounded-2xl shadow-2xl drop-shadow"
+          src={recipe.image}
+          alt={recipe.title}
+        />
+        <div className="bg-zinc-300 p-4 rounded-xl text-gray-700 font-medium text-lg">
+          <p>
+            {sanitizeHtml(recipe.summary, {
+              allowedTags: [],
+              allowedAttributes: {},
+            })}
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
