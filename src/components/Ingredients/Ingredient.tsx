a4d95fcd0e694e39b37b312c7bf020174e8856fa
Pedro Henrique <contact.pedrolima@gmail.com>
2023-02-21 11:00:17 -0300

    stylezed intredients section

--- src/components/Ingredients/Ingredient.tsx
+++ src/components/Ingredients/Ingredient.tsx
@@ -1,15 +1,16 @@
 interface IIngredient {
-    name: string;
-    amount: number;
-    unit: string;
-
+  name: string;
+  amount: number;
+  unit: string;
 }
 
-export function Ingredient({name, amount, unit}: IIngredient) {
-    return (
-        <div className="flex flex-row items-center gap-10">
-            <p>{name}</p>
-            <p>{amount} {unit}</p>
-        </div>
-    )
+export function Ingredient({ name, amount, unit }: IIngredient) {
+  return (
+    <div className="grid grid-cols-2 w-1/2 border-b border-zinc-500 text-zinc-600">
+      <p className="font-semibold text-lg">{name}</p>
+      <div className="font-thin">
+        <span className="font-semibold">{amount}</span> {unit}
+      </div>
+    </div>
+  );
 }
